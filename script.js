99493067a234aac4dd615ba00a99790e2b68d7ae
harshitvarshne <110218954+harshitvarshne@users.noreply.github.com>
2024-06-27 16:23:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbar = document.getElementById("navbar");
+    window.onscroll = function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    };
+});
